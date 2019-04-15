9b508f274519ec071891f2d5a24217d1f8e9bd75
RoyalAgent <libochao12@126.com>
2019-04-15 22:27:26 +0800

    初始化项目

---
+++ iStudio/iStudio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iStudio
+//
+//  Created by cloudin on 2019/4/15.
+//  Copyright © 2019 ICStarta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
